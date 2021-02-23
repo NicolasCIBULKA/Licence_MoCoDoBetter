92707c6e2bca333595f8e83ec22c46ed8a6dfce1
etien <etien@LAPTOP-EE33HJ7N.home>
2021-02-23 20:39:55 +0100

    Update of saving class

--- projetIntegration/src/process/Saving.java
+++ projetIntegration/src/process/Saving.java
@@ -24,7 +24,10 @@ public class Saving {
 	private ArrayList<Association> listAssociation;
 
 	public Saving(String path, MCD mcd) throws SaveWasInteruptedException {
+		listEntity = new ArrayList<Entity>();
+		listAssociation = new ArrayList<Association>();
 		storeMCD(mcd);
+		
 		this.path=path;
 		if(creaFile(path)) {
 			writeEntity();
@@ -39,12 +42,14 @@ public class Saving {
 			Node currentNode = iterator.next();
 			List<Node> connectedNodes = Graphs.neighborListOf(mcd.getMCDGraph(), currentNode);
 			for(Node connectedNode : connectedNodes) {
+				System.out.println("\n\n\n"+connectedNode.toString() +"\n\n");
 				if(connectedNode instanceof Entity) {
 					listEntity.add((Entity) connectedNode);
 				}
 				else if(connectedNode instanceof Association) {
 					listAssociation.add((Association) connectedNode);
 				}
+				
 			}	
 		}
 	}
@@ -65,23 +70,29 @@ public class Saving {
 	
 	private void writeEntity() throws SaveWasInteruptedException {
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(path+".stdd"));
-			writer.write("<Entities>");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(path+".stdd",true));
+			writer.write("<Entities>\n");
 			//null pointer probablement une lecture à vide
-			for(Node entity : listEntity) {
-				writer.write("<Entity>");
-				writer.write("<Name>");
-				writer.write(entity.getName());
-				writer.write("</Name>");
-				for(Attribute attribut : entity.getListAttribute()) {
-					writer.write("<Attribut>");
-					writer.write(attribut.getName()+","+attribut.getType()+","
-				+attribut.isNullable()+","+attribut.isPrimaryKey()+","+attribut.isUnique());
-					writer.write("</Attribut>");
+			if(!listEntity.isEmpty()) {
+				for(Node entity : listEntity) {
+					writer.write("<Entity>\n");
+					writer.write("<Name>\n");
+					writer.write(entity.getName()+"\n");
+					writer.write("</Name>\n");
+					for(Attribute attribut : entity.getListAttribute()) {
+						writer.write("<Attribut>\n");
+						writer.write(attribut.getName()+","+attribut.getType()+","
+					+attribut.isNullable()+","+attribut.isPrimaryKey()+","+attribut.isUnique()+"\n");
+						writer.write("</Attribut>\n");
+					}
+					writer.write("</Entity>\n");
 				}
-				writer.write("</Entity>");
+				
 			}
-			writer.write("</Entities>");
+			else {
+				System.out.println("\n\n\n empty entity\n\n");
+			}
+			writer.write("</Entities>\n");
 			writer.close();
 		}catch(IOException e) {
 			throw new SaveWasInteruptedException("Can't write the entity part of the file");
@@ -90,23 +101,28 @@ public class Saving {
 	
 	private void writeAssociation() throws SaveWasInteruptedException {
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(path+".stdd"));
-			writer.write("<Associations>");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(path+".stdd",true));
+			writer.write("<Associations>\n");
 			//null pointer probablement une lecture à vide
-			for(Node association : listAssociation) {
-				writer.write("<Entity>");
-				writer.write("<Name>");
-				writer.write(association.getName());
-				writer.write("</Name>");
-				for(Attribute attribut : association.getListAttribute()) {
-					writer.write("<Attribut>");
-					writer.write(attribut.getName()+","+attribut.getType()+","
-				+attribut.isNullable()+","+attribut.isPrimaryKey()+","+attribut.isUnique());
-					writer.write("</Attribut>");
+			if(!listAssociation.isEmpty()) {
+				for(Node association : listAssociation) {
+					writer.write("<Entity>\n");
+					writer.write("<Name>\n");
+					writer.write(association.getName()+"\n");
+					writer.write("</Name>\n");
+					for(Attribute attribut : association.getListAttribute()) {
+						writer.write("<Attribut>\n");
+						writer.write(attribut.getName()+","+attribut.getType()+","
+					+attribut.isNullable()+","+attribut.isPrimaryKey()+","+attribut.isUnique()+"\n");
+						writer.write("</Attribut>\n");
+					}
+					writer.write("</Entity>\n");
 				}
-				writer.write("</Entity>");
 			}
-			writer.write("</Associations>");
+			else {
+				System.out.println("\n\n\n empty association\n\n");
+			}
+			writer.write("</Associations>\n");
 			writer.close();
 		}catch(IOException e) {
 			throw new SaveWasInteruptedException("Can't write the association part of the file");
@@ -114,18 +130,29 @@ public class Saving {
 	}
 	private void writeCard() throws SaveWasInteruptedException {
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(path+".stdd"));
-			writer.write("<Cardinalities>");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(path+".stdd",true));
+			writer.write("<Cardinalities>\n");
 			//null pointer probablement une lecture à vide
-			for(Association association : listAssociation) {
-				writer.write("<Cardinality>");
-				for(Cardinality card : association.getCardinalityList()) {
-					writer.write(association.getName()+","+card.getNomEntity()+","+card.getLowValue()+","+
-				card.getHighValue());
+			if(!listAssociation.isEmpty()) {
+				for(Association association : listAssociation) {
+					writer.write("<Cardinality>\n");
+					if(!association.getCardinalityList().isEmpty()) {
+						for(Cardinality card : association.getCardinalityList()) {
+							writer.write(association.getName()+","+card.getNomEntity()+","+card.getLowValue()+","+
+						card.getHighValue()+"\n");
+						}
+					}
+					else {
+						System.out.println("\n\n\n empty cardinatlity\n\n");
+					}
+					writer.write("</Cardinality>\n");
 				}
-				writer.write("</Cardinality>");
 			}
-			writer.write("</Cardinalities>");
+			else {
+				System.out.println("\n\n\n empty association\n\n");
+
+			}
+			writer.write("</Cardinalities>\n");
 			writer.close();
 		}catch(IOException e) {
 			throw new SaveWasInteruptedException("Can't write the cardinality part of the file");
