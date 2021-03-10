a1a275badea3c17a66f978f90910dacab19eff92
fatiatravaille <fatiatravaille@galaxy-s8.home>
2021-03-10 15:39:28 +0100

    test du commit

--- projetIntegration/src/process/MLDManaging.java
+++ projetIntegration/src/process/MLDManaging.java
@@ -26,33 +26,16 @@ public class MLDManaging {
 		//this.entityListToMld = new ArrayList<Entity>();
 		this.mcd = new MCD();
 	}
-
-	
-	/**	
-	public MLDManaging(ArrayList<Entity> entityListToMld) {
-		this.entityListToMld = entityListToMld;
-	}*/
-	
 	
 	public MLDManaging(MLD mld) {
 		this.mld = mld;
 	}
-
-	
-	
-	
-	
-	
-
-//Method		
+		
 	
 	
 	public MCD  newtoMCD(MCD mcd) {
 		ArrayList<Attribute> liste,liste1;
 		String high1 = null;String low1 = null;String high2= null;String low2= null;
-		//MCD mcd=mcdM.getMCD();
-		//UndirectedGraph<Node, DefaultEdge> graph=mcd.getMCDGraph();
-		//ArrayList<Entity> ListOfAllEntities = new ArrayList<Entity>();
 		AbstractGraphIterator<Node, DefaultEdge> iterator = new BreadthFirstIterator<>(mcd.getMCDGraph());
 		while(iterator.hasNext()) {
 			Node currentNode = iterator.next();
@@ -150,14 +133,18 @@ public class MLDManaging {
 					ArrayList<Attribute> liste = new ArrayList<Attribute>();
 					Node n=connectedNodes.get(i);
 					liste=n.getListAttribute();
+					ArrayList<Attribute> liste1=currentNode.getListAttribute();
 					//put the pk first
 					for (Attribute attpk : liste) {
 						if(attpk.isPrimaryKey()) {
 							mldA = new MLDAttribute(attpk.getName(),
 									attpk.getType(), attpk.isNullable(), attpk.isPrimaryKey(),
 									attpk.isUnique(), true, n, attpk);
-							newAttribute.add(mldA);
-							currentNode.getListAttribute().add(mldA);
+							boolean newAtt = true;
+							for(Attribute l :liste1) {
+								if(l.getName().equals(mldA.getName())) {newAtt=false;break;}
+							}
+							if(newAtt==true){currentNode.getListAttribute().add(mldA);}
 						}
 					}
 				}
@@ -193,20 +180,23 @@ public class MLDManaging {
 						Node n=connectedNodes.get(i);
 						liste=n.getListAttribute();
 						//put the pk first
+						ArrayList<Attribute> liste1=currentNode.getListAttribute();
 						for (Attribute attpk : liste) {
 							if(attpk.isPrimaryKey()) {
 								mldA = new MLDAttribute(attpk.getName(),
 										attpk.getType(), attpk.isNullable(), attpk.isPrimaryKey(),
 										attpk.isUnique(), true, n, attpk);	
-								newAttribute.add(mldA);
-								currentNode.getListAttribute().add(mldA);
+								boolean newAtt = true;
+								for(Attribute l :liste1) {
+									if(l.getName().equals(mldA.getName())) {newAtt=false;break;}
+								}
+								if(newAtt==true){currentNode.getListAttribute().add(mldA);}
 							}
 						}
 					}
 					Entity e = new Entity(currentNode.getName(),newAttribute);
 					ListOfAllEntities.add(e);
 				}
-				//else {System.out.println("on ajoute pas");}
 			}
 		}
 		return mcd;
@@ -287,24 +277,18 @@ public class MLDManaging {
 		ArrayList<Entity> ListOf2Entities = new ArrayList<Entity>();
 		ListOf2Entities=addEntityToMLD(mcdM);
 		ListOfAllAssociation=addAssociationToMLD(mcdM);
-		//System.out.println("Debut: "+entityListToMld.size());
 		entityListToMld.addAll(ListOf2Entities);
-		//System.out.println("Après entité:"+entityListToMld.size());
 		entityListToMld.addAll(ListOfAllAssociation);
-		//System.out.println("Après association:"+entityListToMld.size());
 		data.setEntityList(entityListToMld);
 		return entityListToMld;
 		
 	}
 	
 	
-	public MLD newMld(MCDManaging mcdM){
-		//System.out.println(mld.getEntityList().size());
+	public void newMld(MCDManaging mcdM){
 		ArrayList<Entity> AllEntities = new ArrayList<Entity>();
 		AllEntities =ListForMld(mcdM);
 		mld = new MLD(AllEntities);
-		//System.out.println(mld.getEntityList().size());
-		return mld;
 	}
 	
 	
@@ -312,243 +296,7 @@ public class MLDManaging {
 		return mld;
 	}
 	
-	
 
 
-	public ArrayList<Entity>  addAssociation(MCDManaging mcdM) {
-		MLDAttribute mldA;
-		MCD mcd=mcdM.getMCD();
-		UndirectedGraph<Node, DefaultEdge> graph=mcd.getMCDGraph();
-		String high;
-		Entity e1,e2;
-		ArrayList<Entity> ListOfAllEntities = new ArrayList<Entity>();
-		Association association;
-		List<Cardinality> Card;
-		List<String> listCard = new ArrayList<String>();
-		int nb_neighbor;
-		boolean addfk=true;
-		AbstractGraphIterator<Node, DefaultEdge> iterator = new BreadthFirstIterator<>(mcd.getMCDGraph());
-		while(iterator.hasNext()) {
-			Node currentNode = iterator.next();
-			List<Node> connectedNodes = Graphs.neighborListOf(mcd.getMCDGraph(), currentNode);
-			nb_neighbor=connectedNodes.size();
-			//Verify the type of Cardinality
-			if ((currentNode instanceof Association)&&(nb_neighbor==3)) {
-				ArrayList<Attribute> newAttribute=new ArrayList<Attribute>();
-				for(int i = 0 ; i < connectedNodes.size(); i++) {
-					ArrayList<Attribute> liste = new ArrayList<Attribute>();
-					Node n=connectedNodes.get(i);
-					liste=n.getListAttribute();
-					//put the pk first
-					for (Attribute attpk : liste) {
-						if(attpk.isPrimaryKey()) {
-							mldA = new MLDAttribute(attpk.getName(),
-									attpk.getType(), attpk.isNullable(), attpk.isPrimaryKey(),
-									attpk.isUnique(), true, n, attpk);
-							newAttribute.add(mldA);
-						}
-					}
-				}
-				Entity e = new Entity(currentNode.getName(),newAttribute);
-				ListOfAllEntities.add(e);
-			}
-			else if ((currentNode instanceof Association)&&(nb_neighbor==2)) {
-				association=(Association)currentNode;
-				Card=association.getCardinalityList();
-				ListIterator<Cardinality> lItr = Card.listIterator();
-				while (lItr.hasNext()) {
-				    high=lItr.next().getHighValue();
-				    listCard.add(high);
-				}
-				for(String str:listCard) {
-					if(str.equals("N")) {addfk=true;}
-					else {addfk=false;break;}
-				}if(addfk==true) {
-					ArrayList<Attribute> newAttribute=new ArrayList<Attribute>();
-					for(int i = 0 ; i < connectedNodes.size(); i++) {
-						ArrayList<Attribute> liste = new ArrayList<Attribute>();
-						Node n=connectedNodes.get(i);
-						liste=n.getListAttribute();
-						//put the pk first
-						for (Attribute attpk : liste) {
-							if(attpk.isPrimaryKey()) {
-								mldA = new MLDAttribute(attpk.getName(),
-										attpk.getType(), attpk.isNullable(), attpk.isPrimaryKey(),
-										attpk.isUnique(), true, n, attpk);	
-								newAttribute.add(mldA);
-							}
-						}
-					}
-					Entity e = new Entity(currentNode.getName(),newAttribute);
-					ListOfAllEntities.add(e);
-				}
-				else {System.out.println("on ajoute pas");}
-			}
-		}
-		return ListOfAllEntities;
-	}
-	
-	
-	public ArrayList<Entity>  addEntities(MCDManaging mcdM) {
-		ArrayList<Attribute> liste;
-		String high1 = null;
-		String low1 = null;
-		String high2= null;
-		String low2= null;
-		MCD mcd=mcdM.getMCD();
-		UndirectedGraph<Node, DefaultEdge> graph=mcd.getMCDGraph();
-		ArrayList<Entity> ListOfAllEntities = new ArrayList<Entity>();
-		AbstractGraphIterator<Node, DefaultEdge> iterator = new BreadthFirstIterator<>(mcd.getMCDGraph());
-		while(iterator.hasNext()) {
-			Node currentNode = iterator.next();
-			List<Node> connectedNodes = Graphs.neighborListOf(mcd.getMCDGraph(), currentNode);
-			if ((currentNode instanceof Entity)) {
-				//System.out.println("---------");
-				Entity actualEntity=(Entity)currentNode;
-				System.out.println("------Entité actuel:"+actualEntity.getName()+"-----");
-				for (Node actualNode : connectedNodes) {
-					Association actualAssociation=(Association)actualNode;
-					//System.out.println("asso actuel:"+actualAssociation.getName());
-					List<Node> connectedToAssociation = Graphs.neighborListOf(mcd.getMCDGraph(), actualAssociation);			
-					//System.out.println(connectedToAssociation.size());
-					if(connectedToAssociation.size()==3) {/*ne rien faire*/}
-					else if(connectedToAssociation.size()==2) {
-						Entity e2 = null;
-						for (Node j:connectedToAssociation) {
-							Entity k=(Entity)j;
-							if(!actualEntity.equals(k)) {
-								e2=k;
-								System.out.println(e2.getName());
-								
-							}
-						}
-						//System.out.println("asso actuel:"+actualAssociation.getName());
-						//ArrayList<Cardinality> 
-						ArrayList<Cardinality> listCard = actualAssociation.getCardinalityList();
-						for (int i = 0;i<listCard.size();i++) {
-							if(listCard.get(i).getNomEntity().equals(actualEntity.getName())) {
-								high1=listCard.get(i).getHighValue();
-								low1=listCard.get(i).getLowValue();
-								
-							}
-							else {
-								high2=listCard.get(i).getHighValue();
-								low2=listCard.get(i).getLowValue();
-							}
-						}
-						System.out.println(high1);
-						System.out.println(low1);
-						System.out.println(high2);
-						System.out.println(e2.getName());
-						
-						
-						if ((high2.equals("N"))&&(!high1.equals("N"))){
-							
-							liste = new ArrayList<Attribute>();
-							System.out.println("size début:"+actualEntity.getListAttribute().size());
-							liste=e2.getListAttribute();
-							for (Attribute attpk : liste) {
-								if(attpk.isPrimaryKey()) {
-									MLDAttribute mldA = new MLDAttribute(attpk.getName(),
-											attpk.getType(), attpk.isNullable(), false,
-											attpk.isUnique(), true, e2, attpk);	
-									actualEntity.getListAttribute().add(mldA);
-									System.out.println("size début:"+actualEntity.getListAttribute().size());
-								}
-							}
-						}
-						else if((high1.equals("1"))&&(low1.equals("0"))&&(high2.equals("1"))&&(low2.equals("1"))){
-							System.out.println("Cas 3");
-							liste = new ArrayList<Attribute>();
-							System.out.println("size début:"+actualEntity.getListAttribute().size());
-							liste=e2.getListAttribute();
-							for (Attribute attpk : liste) {
-								if(attpk.isPrimaryKey()) {
-									MLDAttribute mldA = new MLDAttribute(attpk.getName(),
-											attpk.getType(), attpk.isNullable(), false,
-											attpk.isUnique(), true, e2, attpk);	
-									actualEntity.getListAttribute().add(mldA);
-									System.out.println("size début:"+actualEntity.getListAttribute().size());
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		return ListOfAllEntities;
-	}
-	
-	
-	
-	
-/**
-	
-	
-	public ArrayList<Entity> ListForMld(MCDManaging mcdM){
-		MLD data = new MLD();
-		ArrayList<Entity> ListOfAllAssociation = new ArrayList<Entity>();
-		ArrayList<Entity> ListOf2Entities = new ArrayList<Entity>();
-		ListOf2Entities=addEntitiesToMLD(mcdM);
-		ListOfAllAssociation=addAssociationToMLD(mcdM);
-		entityListToMld.addAll(ListOfAllAssociation);
-		entityListToMld.addAll(ListOf2Entities);
-		data.setEntityList(entityListToMld);
-		return entityListToMld;
-		
-	}
-	
-	
-	public MLD newMld(MCDManaging mcdM){
-		MLD mld = new MLD();
-		System.out.println(mld.getEntityList().size());
-		ArrayList<Entity> AllEntities = new ArrayList<Entity>();
-		AllEntities =ListForMld(mcdM);
-		mld = new MLD(AllEntities);
-		System.out.println(mld.getEntityList().size());
-		return mld;
-	}
-	
-	public void  testduprograme(MCDManaging mcdM) {
-		MCD mcd= newMCD(mcdM);
-		AbstractGraphIterator<Node, DefaultEdge> iterator = new BreadthFirstIterator<>(mcd.getMCDGraph());
-		while(iterator.hasNext()) {
-			Node currentNode = iterator.next();
-			List<Node> connectedNodes = Graphs.neighborListOf(mcd.getMCDGraph(), currentNode);
-			System.out.println("**********"+currentNode.getName()+"*********");
-			
-			
-			ArrayList<Attribute> att=currentNode.getListAttribute();
-			
-			
-			for(Attribute i:att) {
-				MLDAttribute dog = new MLDAttribute(null, null, false, false, false, false, null, null);
-				if (i instanceof MLDAttribute) {
-					dog = (MLDAttribute) i;
-					System.out.println("--"+dog.getName());
-					//System.out.println("--"+dog.isForeignKey());
-				}
-				else {
-					System.out.println(dog.getName());
-				}
-			}
-			
-			
-			
-			for(int i=0;i<att.size();i++) {
-				MLDAttribute cat = new MLDAttribute(null, null, false, false, false, false, null, null);
-				
-				if (att.get(i) instanceof MLDAttribute) {
-					System.out.println("--"+att.get(i).getName());
-					System.out.println("--"+((MLDAttribute) att.get(i)).isForeignKey());
-				}
-				else {
-					System.out.println(att.get(i).getName());
-				}
-			}
-			
-			
-		}
-	}
-	*/
+
 }
